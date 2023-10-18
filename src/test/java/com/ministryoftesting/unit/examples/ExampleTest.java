128bdaea63e68dbc1461259a32f1b8cfdbf7c616
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2023-10-18 16:04:43 +0100

    Added in example check for lesson 4.2.3

---
+++ src/test/java/com/ministryoftesting/unit/examples/ExampleTest.java
@@ -0,0 +1,16 @@
+// This line indicates that we're placing our code in a specific package called "com.ministryoftesting.unit.examples."
+package com.ministryoftesting.unit.examples;
+
+// We're importing a class called "Test" from the "org.junit.jupiter.api" package.
+import org.junit.jupiter.api.Test;
+
+// We're defining a Java class named "ExampleTest."
+public class ExampleTest {
+
+    // This is a Java annotation that marks a method as a test case.
+    @Test
+    public void shellTest() {
+        // This line prints a message to the console.
+        System.out.println("This is a shell test");
+    }
+}
